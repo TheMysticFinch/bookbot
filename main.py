7069538a66e71cc08df1e527b24b05976ade0e26
The_Mystic_Finch <themysticfinch@gmail.com>
2025-03-04 17:02:42 -0700

    Added get_book_text function

---
+++ main.py
@@ -0,0 +1,16 @@
+def get_book_text(file_path):
+    
+    with open(file_path) as f:
+        files_contents = f.read()
+
+    return files_contents
+
+def main():
+    
+    frankenstein = get_book_text("frankenstein.txt")
+    print(frankenstein)
+
+
+main()
+    
+    
